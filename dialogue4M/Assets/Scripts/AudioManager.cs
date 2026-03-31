b6cd601803454d47f646d0068c55c4fa6f1f662c
Natalia Gabriely <gabriely.j@escolar.ifrn.edu.br>
2026-03-31 09:54:46 -0300

    31/03

---
+++ dialogue4M/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    #region Singleton
+    public static AudioManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+}
